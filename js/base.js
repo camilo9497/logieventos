4b2f08673a4c9d19170060389767a136cf910032
Camilo Aguilera <camilo9497@live.com>
2019-05-08 12:11:44 -0500

    modal mi cuenta

--- js/base.js
+++ js/base.js
@@ -1,9 +1,16 @@
-const iniciarSesion = document.querySelector('.list-menu:last-child')
+const miCuenta = document.querySelector('.list-menu:last-child')
+const iniciarSesion = document.querySelector('.list-menu:nth-child(5)')
 const barra = document.querySelector(".list-menu:nth-child(4)")
 
 iniciarSesion.addEventListener('mouseover', () => {
     barra.classList.toggle('agrandar')
 })
 iniciarSesion.addEventListener('mouseout', () => {
+    barra.classList.toggle('agrandar')
+})
+miCuenta.addEventListener('mouseover', () => {
+    barra.classList.toggle('agrandar')
+})
+miCuenta.addEventListener('mouseout', () => {
     barra.classList.toggle('agrandar')
 })
