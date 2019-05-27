0a9f54c38901cce4a250d1603390cf1cefa15e3b
Camilo Aguilera <camilo9497@live.com>
2019-05-26 21:25:04 -0500

    Todo del paso a paso y menu

--- js/base.js
+++ js/base.js
@@ -2,15 +2,18 @@ const miCuenta = document.querySelector('.list-menu:last-child')
 const iniciarSesion = document.querySelector('.list-menu:nth-child(5)')
 const barra = document.querySelector(".list-menu:nth-child(4)")
 
+const botonMenu = document.getElementById('contenedor-menu')
+const modal = document.getElementById('modal-menu')
+
 iniciarSesion.addEventListener('mouseover', () => {
     barra.classList.toggle('agrandar')
 })
 iniciarSesion.addEventListener('mouseout', () => {
     barra.classList.toggle('agrandar')
 })
-miCuenta.addEventListener('mouseover', () => {
-    barra.classList.toggle('agrandar')
+
+
+botonMenu.addEventListener('click', (e) => {
+    modal.classList.toggle('mostrar-menu')
 })
-miCuenta.addEventListener('mouseout', () => {
-    barra.classList.toggle('agrandar')
-})
+
